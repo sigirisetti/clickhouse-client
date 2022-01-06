688027009d6b009d4fee097b61702bf2b614592b
Sigirisetty Surya Kiran <sigirisetti@gmail.com>
2022-01-06 12:04:13 +0800

    array column type handling

--- clickhouse-db-common/src/main/java/com/ssk/clickhouse/com/ssk/clickhouse/dao/TableDao.java
+++ clickhouse-db-common/src/main/java/com/ssk/clickhouse/com/ssk/clickhouse/dao/TableDao.java
@@ -9,11 +9,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TableDao {
 
-    private final String SELECT_TABLE_DATA_SQL = "SELECT * FROM %s.%s LIMIT 1000";
+    private final String SELECT_TABLE_DATA_SQL = "SELECT %s FROM %s.%s LIMIT 1000";
 
     private final ClickHouseDataSource dataSource;
 
@@ -24,7 +27,12 @@ public class TableDao {
     public List<List<Object>> getTableData(Table table) {
         List<List<Object>> tableData = new ArrayList<>(1000);
         try (ClickHouseConnection connection = dataSource.getConnection()) {
-            String sql = String.format(SELECT_TABLE_DATA_SQL, table.getDatabase(), table.getName());
+
+            String cols = table.getColumn().stream().map(c -> c.getName()).collect(Collectors.joining(", "));
+
+            String sql = String.format(SELECT_TABLE_DATA_SQL, cols, table.getDatabase(), table.getName());
+
+
             System.out.println("Firing sql : " + sql);
             PreparedStatement ps = connection.prepareStatement(sql);
             int n = table.getColumn().size();
@@ -32,8 +40,14 @@ public class TableDao {
                 while (rs.next()) {
                     List<Object> row = new ArrayList<>(n);
                     tableData.add(row);
-                    for(Column c : table.getColumn()) {
-                        row.add(rs.getObject(c.getName()));
+                    for (Column c : table.getColumn()) {
+                        if (c.getType().startsWith("Array")) {
+                            row.add(Arrays.stream((Object[])rs.getArray(c.getName()).getArray())
+                                    .filter(Objects::nonNull)
+                                    .map(String::valueOf).collect(Collectors.joining(", ")));
+                        } else {
+                            row.add(rs.getObject(c.getName()));
+                        }
                     }
                 }
             }
